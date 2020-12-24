5b3668f0d53761e88a94103f5d9782934046c6cd
Viktor Blazhkevych <blazhkevych.va@gmail.com>
2020-12-24 09:44:56 +0200

    Add project files.

---
+++ 23122020form/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace _23122020form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.Blue;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Ivan",
+            "Stepan",
+            "Ira",
+            "Anna"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 268);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 286);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(184, 72);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.ForeColor = System.Drawing.Color.Blue;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(537, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(122, 268);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.DrawItem);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Ivan",
+            "Stepan",
+            "Ira",
+            "Anna",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60"});
+            this.checkedListBox1.Location = new System.Drawing.Point(278, 12);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(240, 139);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(278, 292);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(65, 48);
+            this.buttonCopy.TabIndex = 5;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 48);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(423, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Inversion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "agent.png");
+            this.imageList1.Images.SetKeyName(1, "airpay.png");
+            this.imageList1.Images.SetKeyName(2, "bonus.png");
+            this.imageList1.Images.SetKeyName(3, "chast.png");
+            this.imageList1.Images.SetKeyName(4, "deposit.png");
+            this.imageList1.Images.SetKeyName(5, "kopilka.png");
+            this.imageList1.Images.SetKeyName(6, "privat24.png");
+            this.imageList1.Images.SetKeyName(7, "sms.png");
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.ImageIndex = 0;
+            this.checkBox1.ImageList = this.imageList1;
+            this.checkBox1.Location = new System.Drawing.Point(54, 388);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 80);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoCheck = false;
+            this.checkBox2.ImageIndex = 1;
+            this.checkBox2.ImageList = this.imageList1;
+            this.checkBox2.Location = new System.Drawing.Point(158, 388);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 67);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoCheck = false;
+            this.checkBox3.ImageIndex = 2;
+            this.checkBox3.ImageList = this.imageList1;
+            this.checkBox3.Location = new System.Drawing.Point(278, 388);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(83, 67);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.ImageIndex = 4;
+            this.button4.ImageList = this.imageList1;
+            this.button4.Location = new System.Drawing.Point(423, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 70);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Click";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 482);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(308, 30);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(52, 529);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(309, 45);
+            this.trackBar1.TabIndex = 13;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(325, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 547);
+            this.vScrollBar1.TabIndex = 14;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 547);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(342, 17);
+            this.hScrollBar1.TabIndex = 15;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitContainer1.Location = new System.Drawing.Point(678, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.vScrollBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.hScrollBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(518, 564);
+            this.splitContainer1.SplitterDistance = 172;
+            this.splitContainer1.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(172, 564);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 564);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 23122020form/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23122020form
+{
+    public partial class Form1 : Form
+    {
+        Timer timerAnim = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timerAnim.Interval = 10;
+            timerAnim.Tick += TimerAnim_Tick;
+        }
+
+        int CountAnim = 0;
+        bool OnButton = false;
+        private void TimerAnim_Tick(object sender, EventArgs e)
+        {
+            int k = OnButton ? 1 : -1;
+            CountAnim += k;
+            if (OnButton)
+            {
+                if (CountAnim > 5)
+                    timerAnim.Stop();
+            }
+            else
+            {
+                if (CountAnim < 1)
+                    timerAnim.Stop();
+            }
+            button4.Size = new Size(button4.Size.Width + k, button4.Size.Height + k);
+        }
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button4.ImageIndex = 5;
+            OnButton = true;
+            timerAnim.Start();
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4.ImageIndex = 4;
+            OnButton = false;
+            timerAnim.Start();
+        }
+
+        int k = 0;
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            // string text = "123";
+            string text = (sender as ListBox)?.Items[e.Index].ToString();
+
+            Brush brush = e.Index % 2 == 0 ? Brushes.Red : Brushes.Blue;
+            e.Graphics.DrawString(text,
+                e.Font,
+                brush,
+                e.Bounds,
+                StringFormat.GenericDefault);
+            e.DrawFocusRectangle();
+
+            Text = "" + ++k;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(11345);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                listBox2.Items.Add(random.Next(-10, 11));
+            }
+        }
+        private void DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            // string text = "123";
+            if (sender is ListBox LB && LB.Items.Count > 0)
+            {
+                if (LB.Items[e.Index] is Int32 n)
+                {
+                    Brush brush = (n > 0 ? Brushes.Green : (n < 0 ? Brushes.Red : Brushes.Black));
+                    e.Graphics.DrawString(
+                        "" + n,
+                        e.Font,
+                        brush,
+                        e.Bounds,
+                        StringFormat.GenericDefault);
+                }
+            }
+            e.DrawFocusRectangle();
+            Text = "" + ++k;
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                    listBox1.Items.Add(checkedListBox1.Items[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < checkedListBox1.Items.Count; i++)            
+            //    checkedListBox1.SetItemChecked(i, !checkedListBox1.GetItemChecked(i));
+
+            checkedListBox1.SetItemCheckState(1, CheckState.Checked);
+            checkedListBox1.SetItemCheckState(2, CheckState.Unchecked);
+            checkedListBox1.SetItemCheckState(3, CheckState.Indeterminate);
+
+        }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkBox1.ImageIndex++;
+        }
+    }
+}
